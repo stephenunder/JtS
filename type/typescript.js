d8e795441ece54ae5107523ca31d5276b80ee39e
Stephen <41934385+stephenunder@users.noreply.github.com>
2019-03-23 22:08:03 -0500

    more types

--- type/typescript.js
+++ type/typescript.js
@@ -31,3 +31,49 @@ var Size;
     Size[Size["Large"] = 3] = "Large";
 })(Size || (Size = {}));
 var sizeName = Size[2];
+// Any - !! Be Careful
+var whatever = "aghh noo!";
+whatever = basket;
+// void
+var sing = function () {
+    console.log("lalalala");
+};
+// never
+var error = function () {
+    throw Error("oops");
+};
+var fightRobotArmy = function (robots) {
+    console.log("FIGHT!");
+};
+fightRobotArmy({ count: 1, type: "dragon" });
+var fightRobotArmy2 = function (robots) {
+    console.log("FIGHT!");
+};
+var dog = {};
+dog.count;
+// Function
+var fightRobotArmy3 = function (robots) {
+    console.log("FIGHT!");
+};
+var fightRobotArmy4 = function (robots) {
+    console.log("FIGHT!");
+    return 5;
+};
+// Classes
+var Animal = /** @class */ (function () {
+    function Animal(sound) {
+        this.sing = "lalalala";
+        this.sing = sound;
+    }
+    Animal.prototype.greet = function () {
+        return "Hello " + this.sing;
+    };
+    return Animal;
+}());
+var lion = new Animal("RAWR");
+lion.greet();
+// Union
+var confused = "hello"; // or 5 for example
+// Type inference
+var x = 4;
+x = "hey"; // not explicitly stated, but TS inferred x as number
--- type/typescript.ts
+++ type/typescript.ts
@@ -36,3 +36,75 @@ basket = ["basketball", 5];
 // Enum
 enum Size { Small = 1, Medium = 2, Large = 3}
 let sizeName: string = Size[2];
+
+// Any - !! Be Careful
+let whatever: any = "aghh noo!";
+whatever = basket;
+
+// void
+let sing = (): void => {
+  console.log("lalalala");
+}
+
+// never
+let error = (): never => {
+  throw Error("oops");
+}
+
+// interface
+interface RobotArmy {
+  count: number,
+  type: string,
+  magic?: string
+}
+
+let fightRobotArmy = (robots: RobotArmy) => {
+  console.log("FIGHT!")
+}
+fightRobotArmy({count: 1, type: "dragon"})
+
+let fightRobotArmy2 = (robots: {count: number, type: string, magic: string}) => {
+  console.log("FIGHT!");
+}
+
+// Type Assertion
+interface CatArmy {
+  count: number,
+  type: string,
+  magic: string
+}
+
+let dog = {} as CatArmy
+dog.count
+
+// Function
+let fightRobotArmy3 = (robots: RobotArmy): void => {
+  console.log("FIGHT!");
+}
+
+let fightRobotArmy4 = (robots: {count: number, type: string, magic: string}): number => {
+  console.log("FIGHT!");
+  return 5;
+}
+
+// Classes
+class Animal {
+  private sing: string = "lalalala";
+  constructor(sound: string) {
+    this.sing = sound;
+  }
+
+  greet(): string {
+    return `Hello ${this.sing}`;
+  }
+}
+
+let lion = new Animal("RAWR");
+lion.greet()
+
+// Union
+let confused: string | number = "hello"; // or 5 for example
+
+// Type inference
+let x = 4;
+x = "hey" // not explicitly stated, but TS inferred x as number
